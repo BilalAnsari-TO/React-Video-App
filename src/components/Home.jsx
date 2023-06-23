7b6853a2dbfd37b23d8ac8caaaa530fe749f3598
BilalAnsari-TO <m.bilal.ansari@techoverflow.io>
2023-06-23 16:44:50 +0500

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import { Box, Heading, Image,Container,Text,Stack } from '@chakra-ui/react'
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-
-
-import img1 from "../assets/1.jpg"
-import img2 from "../assets/2.jpg"
-import img3 from "../assets/3.jpg"
-import img4 from "../assets/4.jpg"
-import img5 from "../assets/5.png"
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
 
 const headingOptions = {
-    pos: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%,-50%)',
-    textTransform: 'uppercase',
-    p: '4',
-    size: '4xl',
-  };
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
 
 const Home = () => {
-  return <Box>
-        <MyCarousel/>
+  return (
+    <Box>
+      <MyCarousel />
 
-        <Container maxW={'container.xl'} minH={'100vh'} p="16">
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
         <Heading
           textTransform={'uppercase'}
           py="2"
@@ -66,38 +65,46 @@ const Home = () => {
           </Text>
         </Stack>
       </Container>
-        
     </Box>
-  
+  );
 };
-const MyCarousel= () =>(
-    <Carousel autoPlay infiniteLoop interval={1000} showArrows={false} showThumbs={false} showStatus={false} >
-        <Box w={'full'} h={"100vh"}>
-            <Image src={img1} />
-            <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions} >
-                Watch the future 
-                </Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image src={img2} />
-            <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions} >
-                Future is gaming
-                </Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image src={img3} />
-            <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions} >
-                Gamming on Console 
-                </Heading>
-        </Box>
-        <Box w={'full'} h={"100vh"}>
-            <Image src={img4} />
-            <Heading bgColor={'whiteAlpha.600'} color={'white'} {...headingOptions} >
-                Night life is cool
-                </Heading>
-        </Box>
-        
-    </Carousel>
-)
- 
-export default Home
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
